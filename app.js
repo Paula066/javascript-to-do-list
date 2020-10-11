95bdfb08fc4cfda3f96b372c3c9d63f6d7b5958a
Paulina <paulina@mymenu.pl>
2020-10-11 12:59:23 +0200

    fix addTask function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-const ul = document.createElement('ul');
-const li = document.createElement('li');
+
 
 addTask = (e) => {
     e.preventDefault();
     const titleTask =  input.value;
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
     ul.appendChild(li);
     document.body.appendChild(ul);
     li.textContent =  titleTask;
