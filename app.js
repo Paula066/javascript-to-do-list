524ccf9c76334a9c0b834036a8dbc61b7522412e
Paulina <paulina@mymenu.pl>
2020-10-12 22:08:55 +0200

    change on object programming, addTask function and remove

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
-const input = document.querySelector('input');
-const ul = document.createElement('ul');
+// const addTask = (e) => {
+//
+//     if (e.keyCode === 13) {
+//         const titleTask = e.target.value;
+//         const li = document.createElement('li');
+//         if (titleTask === '') return;
+//         li.textContent = titleTask;
+//         ul.appendChild(li);
+//     }
+// }
 
+class App {
+    constructor() {
+        this.events();
+    }
 
-const addTask = (inputValue) => {
-    const titleTask =  input.value;
-    const li = document.createElement('li');
-    if (titleTask === '') return ;
-    ul.appendChild(li);
-    document.body.appendChild(ul);
-    li.textContent =  titleTask;
-    input.value = "";
+    events() {
+        this.handlers.input.addEventListener('keydown', this.addTask)
+        this.handlers.list.forEach(list => list.addEventListener('click', this.removeTask))
+    }
+
+    addTask = (e) => {
+        if (e.code !== "Enter") return;
+        const value = this.handlers.input.value;
+        const task = new Task(value);
+        if (task.isTaskValid()) {
+            this.handlers.list.forEach(li => li.appendChild(task.getElement()));
+        }
+    }
+
+    removeTask = (e) => {
+        e.target.parentNode.removeChild(e.target)
+    }
+
+    get handlers() {
+        return {
+            input: document.querySelector('input'),
+            list: document.querySelectorAll('#listOfTask')
+        };
+    }
+
 }
 
-input.addEventListener('input', addTask)
+class Task {
+    constructor(name) {
+        this.name = name;
+    }
+
+    isTaskValid() {
+        return !!this.name.replace(/\s/g, '').length;
+    }
+
+    getElement() {
+        const li = document.createElement('li');
+        li.textContent = this.name;
+        return li;
+    }
+}
+
+new App();
+
+
 
